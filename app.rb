b2dbaf3da71c5ae68065a989e4c69448a3ea73ef
Ruta <ruta@baltiejus.com>
2016-04-26 09:38:18 +0100

    added cat to webpage

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   'Hello Elia'
 end
+
+get '/secret' do
+  "This is a secret!"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+  </div>"
+end
+
+set :session_secret, 'super secret'
