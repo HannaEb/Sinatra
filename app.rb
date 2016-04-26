6f342526eece3c584de55265e662ef3644010ffb
Ruta <ruta@baltiejus.com>
2016-04-26 10:11:22 +0100

    added params

--- app.rb
+++ app.rb
@@ -8,7 +8,14 @@ get '/secret' do
   "This is a secret!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Luni", "Marley", "Zirnis"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
 
